6849b7bf964012b16d7f7efaafb35a4a0f6d9c7e
Calem Bendell <calem.j.bendell@gmail.com>
2016-05-23 02:39:42 -0500

    minimization experiment

---
+++ site_minify.py
@@ -0,0 +1,17 @@
+import gzip
+import os
+import shutil
+
+supported_filetypes = [".js", ".css", ".html"]
+
+for path, subdirs, files in os.walk("."):
+    for name in files:
+        resolved = os.path.join(path, name)
+        type_flag = False
+        for filetype in supported_filetypes:
+            if resolved.endswith(filetype):
+                type_flag = True
+        if type_flag:
+            with open(resolved, 'rb') as f_in:
+                with gzip.open(resolved + ".gz", 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
